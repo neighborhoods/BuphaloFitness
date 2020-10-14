ffc8cecffdf6c2a2eaec0a8a07755b11fc365e45
Alex Berryman <alexbman@gmail.com>
2020-10-14 12:37:14 -0500

    Duplicate demo3 into demo4

---
+++ Demo4/bin/buphalo.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+cd "$(dirname "$0")" || exit
+cd ..
+
+Neighborhoods_Buphalo_V1_TargetApplication_BuilderInterface__SourceDirectoryPath=$PWD/src \
+Neighborhoods_Buphalo_V1_TargetApplication_BuilderInterface__FabricationDirectoryPath=$PWD/fab \
+Neighborhoods_Buphalo_V1_TargetApplication_BuilderInterface__NamespacePrefix=Neighborhoods\\BuphaloFitness\\Demo4 \
+Neighborhoods_Buphalo_V1_TemplateTree_Map_Builder_FactoryInterface__TemplateTreeDirectoryPaths=$PWD/templates \
+php vendor/bin/buphalo && composer dump-autoload -a
