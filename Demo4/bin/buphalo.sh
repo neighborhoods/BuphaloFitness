7e60be9adfb630005602132d2029d53f9abe140b
Alex Berryman <alexbman@gmail.com>
2020-10-14 12:38:09 -0500

    use a modern buphalo.sh file

--- Demo4/bin/buphalo.sh
+++ Demo4/bin/buphalo.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
+# Bash instructions to Buphalo Walkscore Service.
 
 cd "$(dirname "$0")" || exit
 cd ..
+# shellcheck disable=SC2034
 
 Neighborhoods_Buphalo_V1_TargetApplication_BuilderInterface__SourceDirectoryPath=$PWD/src \
-Neighborhoods_Buphalo_V1_TargetApplication_BuilderInterface__FabricationDirectoryPath=$PWD/fab \
+Neighborhoods_Buphalo_V1_TargetApplication_BuilderInterface__FabricationDirectoryPath=$PWD/fab-buphalo \
+Neighborhoods_Buphalo_V1_TargetApplication_BuilderInterface__NamespacePrefix=Neighborhoods\\WalkScoreService\\ \
 Neighborhoods_Buphalo_V1_TargetApplication_BuilderInterface__NamespacePrefix=Neighborhoods\\BuphaloFitness\\Demo4 \
-Neighborhoods_Buphalo_V1_TemplateTree_Map_Builder_FactoryInterface__TemplateTreeDirectoryPaths=$PWD/templates \
-php vendor/bin/buphalo && composer dump-autoload -a
+Neighborhoods_Buphalo_V1_FabricationFile_Map_BuilderInterface__FinderFileNames="*.buphalo.v1.fabrication.yml" \
+vendor/bin/buphalo
