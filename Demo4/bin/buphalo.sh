a2e0e2a80e8314438db68a147f62db00ea77867b
Alex Berryman <alexbman@gmail.com>
2020-10-14 12:44:43 -0500

    use a modern buphalo.sh file that is called from `composer install`

--- Demo4/bin/buphalo.sh
+++ Demo4/bin/buphalo.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Bash instructions to Buphalo Walkscore Service.
+# Bash instructions to Buphalo Demo
 
 cd "$(dirname "$0")" || exit
 cd ..
@@ -7,7 +7,7 @@ cd ..
 
 Neighborhoods_Buphalo_V1_TargetApplication_BuilderInterface__SourceDirectoryPath=$PWD/src \
 Neighborhoods_Buphalo_V1_TargetApplication_BuilderInterface__FabricationDirectoryPath=$PWD/fab-buphalo \
-Neighborhoods_Buphalo_V1_TargetApplication_BuilderInterface__NamespacePrefix=Neighborhoods\\WalkScoreService\\ \
 Neighborhoods_Buphalo_V1_TargetApplication_BuilderInterface__NamespacePrefix=Neighborhoods\\BuphaloFitness\\Demo4 \
+Neighborhoods_Buphalo_V1_TemplateTree_Map_Builder_FactoryInterface__TemplateTreeDirectoryPaths=$PWD/vendor/neighborhoods/buphalo/template-tree/V1 \
 Neighborhoods_Buphalo_V1_FabricationFile_Map_BuilderInterface__FinderFileNames="*.buphalo.v1.fabrication.yml" \
 vendor/bin/buphalo
